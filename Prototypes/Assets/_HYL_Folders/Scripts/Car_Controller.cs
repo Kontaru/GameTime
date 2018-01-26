073beb430210d115d01bbca2c90c48716030d58b
Kontaru <drwafflesxd@gmail.com>
2018-01-26 16:36:55 +0000

    HYL: NavMesh test

---
+++ Prototypes/Assets/_HYL_Folders/Scripts/Car_Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Car_Controller : MonoBehaviour {
+
+    public GameObject destination;
+    NavMeshAgent nav_agent;
+
+	// Use this for initialization
+	void Start () {
+        nav_agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nav_agent.destination = destination.transform.position;
+    }
+}
