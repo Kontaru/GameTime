9eeb9493d2595d2aa008eeeb8024e1bdb47829a0
Kontaru <drwafflesxd@gmail.com>
2018-01-29 01:11:36 +0000

    HYL: Top speed handler (+ gear shifting needs to be implemented)

---
+++ Prototypes/Assets/_HYL_Folders/Scripts/Car Controls/InterfaceHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InterfaceHandler : MonoBehaviour {
+
+    Player Car;
+
+    public TextMeshProUGUI Speed;
+
+    // Use this for initialization
+    void Start () {
+        Car = GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Speed.text = string.Format("" + Player.MPH);
+	}
+}
--- Prototypes/Assets/_HYL_Folders/Scripts/Car Controls/Player.cs
+++ Prototypes/Assets/_HYL_Folders/Scripts/Car Controls/Player.cs
@@ -5,36 +5,50 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour {
 
-    public Car settings;
+    public Car car_Settings;
 
     public enum ShiftState
     {
         None,
         Cruising,
-        Draft,
+        Drag,
         Drift,
         Crashed
     }
 
+    public enum Gear
+    {
+        None,
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6
+    }
+
     public ShiftState State;
+    public Gear CurrentGear;
 
     public GameObject destination;
     public static NavMeshAgent nav_agent;
-    CruiseMode Cruising;
     DraftMode Drafting;
     DriftMode Drifting;
 
-    public float MPH;
-    public float KPH;
+    public static float MPH;
+    public static float KPH;
+    public float BL_speedLimit;
 
     // Use this for initialization
     void Start()
     {
         nav_agent = GetComponent<NavMeshAgent>();
 
-        Cruising = GetComponent<CruiseMode>();
         Drafting = GetComponent<DraftMode>();
         Drifting = GetComponent<DriftMode>();
+
+        CurrentGear = Gear.One;
+        nav_agent.speed = 0;
     }
 
     // Update is called once per frame
@@ -42,32 +56,90 @@ public class Player : MonoBehaviour {
     {
         nav_agent.destination = destination.transform.position;
 
-        MPH = nav_agent.speed * 2.237f;
-        KPH = nav_agent.speed * 3.6f;
+        SetSpeedLimit();
+        Accelerate();
 
-        if (State == ShiftState.Draft)
+        if (State == ShiftState.Drag)
         {
             Drifting.Off();
             Drafting.On();
-            Cruising.Off();
         }else if (State == ShiftState.Drift)
         {
             Drifting.On();
             Drafting.Off();
-            Cruising.Off();
         }
         else if (State == ShiftState.Cruising)
         {
             Drifting.Off();
             Drafting.Off();
-            Cruising.On();
+            Cruising();
         }
         else if (State == ShiftState.Crashed)
         {
             Drifting.Off();
             Drafting.Off();
-            Cruising.Off();
         }
 
+    }
+
+    void SetSpeedLimit()
+    {
+        if (State == ShiftState.Drag)
+        {
+            if (CurrentGear == Gear.Six)
+            {
+                BL_speedLimit = car_Settings.gears[5].speedLimit;
+            }
+            else if (CurrentGear == Gear.Five)
+            {
+                BL_speedLimit = car_Settings.gears[4].speedLimit;
+            }
+            else if (CurrentGear == Gear.Four)
+            {
+                BL_speedLimit = car_Settings.gears[3].speedLimit;
+            }
+            else if (CurrentGear == Gear.Three)
+            {
+                BL_speedLimit = car_Settings.gears[2].speedLimit;
+            }
+            else if (CurrentGear == Gear.Two)
+            {
+                BL_speedLimit = car_Settings.gears[1].speedLimit;
+            }
+            else if (CurrentGear == Gear.One)
+            {
+                BL_speedLimit = car_Settings.gears[0].speedLimit;
+            }
+        }
+    }
+
+    void Accelerate()
+    {
+        if (MPH > BL_speedLimit)
+            nav_agent.speed = Random.Range(BL_speedLimit / 2.237f, BL_speedLimit / 2.237f + 2.0f);
+        else
+            nav_agent.speed += car_Settings.acceleration * Time.deltaTime;
+
+        MPH = nav_agent.speed * 2.237f;
+        KPH = nav_agent.speed * 3.6f;
+    }
+
+    void Cruising()
+    {
+        
+    }
+
+    void ShiftUp()
+    {
+        int vGear = (int)CurrentGear;
+        vGear += 1;
+        CurrentGear = (Gear)vGear;
+    }
+
+    void ShiftDown()
+    {
+        int vGear = (int)CurrentGear;
+        vGear -= 1;
+        CurrentGear = (Gear)vGear;
     }
 }
