0586895fce3405a25eeeb13daf06bee9ba215fd6
Oliver Knight <oliknight95@gmail.com>
2018-01-29 20:51:01 +0000

    OK: Menu Scenes and game scenes added
    Menu struture prototyped in basic form so that game can be build around it.

---
+++ Prototypes/Assets/_OK_Folders/Scripts/OK_SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OK_SceneLoader : MonoBehaviour {
+
+    public int in_SceneNum;
+    
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(in_SceneNum);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
